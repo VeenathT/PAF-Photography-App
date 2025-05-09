e239601eaa84be4ecef7f58e65ab39424e465be5
VeenathT <veenaththamuditha@gmail.com>
2025-05-09 23:46:12 +0530

    update navbar brand name to remove space

--- frontend/src/Components/Navbar/index.jsx
+++ frontend/src/Components/Navbar/index.jsx
@@ -52,7 +52,7 @@ function Navbar() {
       <nav className="navbar navbar-expand-lg navbar-light bg-light p-3">
         <div className="container-fluid">
           <Link className="navbar-brand" to="/">
-            Photography Life
+            PhotographyLife
           </Link>
           <button
             className="navbar-toggler"
